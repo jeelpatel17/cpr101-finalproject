1df00b33cf61dbc505646d6d9e59c1f070722bff
hguo49 <110922325+hguo49@users.noreply.github.com>
2022-08-12 13:48:09 +0000

    Add files via upload

---
+++ tokenizing.h
@@ -0,0 +1,6 @@
+#ifndef _TOKENIZING_H_//check if TOKENIZING_H has been defined 
+#define _TOKENIZING_H_//include the header with the function prototype
+#include <stdio.h>//includes stdio.h function
+#include <string.h>//contains string function
+void tokenizing(void);//function declaration 
+#endif//the function determines if the #ifndef completed
